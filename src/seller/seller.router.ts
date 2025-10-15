151a1e1fea9b5fa307757d66738edca0db165d9a
Charles-349 <wamahiucharles123@gmail.com>
2025-10-15 21:02:03 +0300

    user updates

--- src/seller/seller.router.ts
+++ src/seller/seller.router.ts
@@ -1,18 +1,36 @@
 
-import { customerRoleAuth } from "../middleware/bearAuth";
-import { 
-  createSeller
-} from "./seller.controller";
+// import { customerRoleAuth } from "../middleware/bearAuth";
+// import { 
+//   createSeller
+// } from "./seller.controller";
+// import { Express } from "express";
+
+// const seller = (app: Express) => {
+//   // Register Seller
+//   app.route("/seller/register").post(customerRoleAuth,async (req, res, next) => {  
+//     try {
+//       await createSeller(req, res);
+//     } catch (error) {
+//       next(error);
+//     }
+//   });
+// } 
+// export default seller;   
+
+
+
 import { Express } from "express";
+import { createSeller } from "./seller.controller";
 
 const seller = (app: Express) => {
-  // Register Seller
-  app.route("/seller/register").post(customerRoleAuth,async (req, res, next) => {  
+  // Register Seller 
+  app.route("/seller/register").post(async (req, res, next) => {
     try {
       await createSeller(req, res);
     } catch (error) {
       next(error);
     }
   });
-} 
-export default seller;                                                                                                                                      
+};
+
+export default seller;
