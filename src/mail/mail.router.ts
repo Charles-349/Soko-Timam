c9828d1ac2fa84935c2c083fc7be83c504dae530
Charles-349 <wamahiucharles123@gmail.com>
2025-10-18 13:20:45 +0300

    bank updates

--- src/mail/mail.router.ts
+++ src/mail/mail.router.ts
@@ -1,16 +1,10 @@
 import { Express } from "express";
-import { sendEmail } from "../mailer/mailer";
+import { sendTestEmailController } from "./mail.controller";
 
 const mail = (app: Express) => {
   app.route("/mail/test").post(async (req, res, next) => {
     try {
-      const { email } = req.body;
-      const response = await sendEmail(
-        email,
-        "Soko Timam Test Email",
-        "Hello! This is a test email from your Render app."
-      );
-      res.status(200).json({ message: "Email sent", response });
+      await sendTestEmailController(req, res);
     } catch (error) {
       next(error);
     }
