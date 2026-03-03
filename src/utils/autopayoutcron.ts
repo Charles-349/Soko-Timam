b8418649084260de5eb4b28123af9ce85e1f147d
Charles-349 <wamahiucharles123@gmail.com>
2026-03-03 14:06:47 +0300

    escrow process

--- src/utils/autopayoutcron.ts
+++ src/utils/autopayoutcron.ts
@@ -1,8 +1,8 @@
-import { autoPayoutService } from "../wallet/wallet.service";
-import cron from "node-cron";
+// import { autoPayoutService } from "../wallet/wallet.service";
+// import cron from "node-cron";
 
-// Runs every day at 2:00 AM
-cron.schedule("0 2 * * *", async () => {
-  console.log("Running daily auto-payout job...");
-  await autoPayoutService();
-});
+// // Runs every day at 2:00 AM
+// cron.schedule("0 2 * * *", async () => {
+//   console.log("Running daily auto-payout job...");
+//   await autoPayoutService();
+// });
